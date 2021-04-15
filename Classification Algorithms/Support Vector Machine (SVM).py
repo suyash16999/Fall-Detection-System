7eb0691429e0a9403370d77d1fc5da9eacb39095
suyash16999 <suyash16999@gmail.com>
2021-04-15 14:44:03 +0530

    Serialized model for testing on RPi

--- Classification Algorithms/Support Vector Machine (SVM).py
+++ Classification Algorithms/Support Vector Machine (SVM).py
@@ -1,5 +1,4 @@
 # Support Vector Machine (SVM)
-
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,7 +12,7 @@ X = dataset.iloc[:, 1:].values
 y = dataset.iloc[:, 0].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
@@ -24,11 +23,17 @@ X_test = sc.transform(X_test)
 
 # Fitting SVM to the Training set
 from sklearn.svm import SVC
-classifier = SVC(C=1600, kernel = 'rbf', random_state = 0)
+classifier = SVC(C=40, kernel = 'rbf', random_state = 0)
 classifier.fit(X_train, y_train)
 
+from sklearn.externals import joblib
+# Save the model as a pickle in a file
+joblib.dump(classifier, 'SVM.pkl') 
+# Load the pickled model
+Test_Classifier = joblib.load('SVM.pkl') 
+
 # Predicting the Test set results
-y_pred = classifier.predict(X_test)
+y_pred = Test_Classifier.predict(X_test)
 
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
@@ -36,3 +41,7 @@ cm = confusion_matrix(y_test, y_pred)
 print(cm)
 from sklearn.metrics import classification_report
 print(classification_report(y_test, y_pred))
+
+
+
+
